8701f2b8044d139f515e3f8c942d3c881a6e6160
MickeyLLG <liuruicong1118@qq.com>
2025-11-28 13:30:20 +0900

    Update configs and pretrain script

--- exps/pretrain.sh
+++ exps/pretrain.sh
@@ -1,7 +1,7 @@
 export PYTHONPATH="."
 export PYOPENGL_PLATFORM=osmesa
 
-# Recommendation: Training with 3 GPUs, batch size 256, learning rate 1e-4, 5 epochs.
+# Recommendation: Training with 3 GPUs, batch size 256, learning rate 1e-4, 4 epochs.
 CUDA_VISIBLE_DEVICES=0,1,2 torchrun --nproc_per_node=3 main.py --config_file configs/config/clip_base.yml
 CUDA_VISIBLE_DEVICES=0,1,2 torchrun --nproc_per_node=3 main.py --config_file configs/config/clip_base_eval.yml --eval
 
